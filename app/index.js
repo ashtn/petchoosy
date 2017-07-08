50bca516a23c85b77c62376dabfe1dc378fd9c0d
Ashtn <ashtonehorton@gmail.com>
2017-07-07 19:18:36 -0700

    render text

---
+++ app/index.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css'); // everything included in the index.css will be included in the application when it bundles // aligns with css and style-loader in webpack
+
+//state
+//lifecycle
+// UI - render(){return JSX}
+class App extends React.Component {
+  render(){
+    return (
+      <div>[G]otchoo</div>
+
+    )
+  }
+}
+//what to render and where to render to
+ReactDOM.render(
+  <App /> ,
+  document.getElementById('app')
+);
