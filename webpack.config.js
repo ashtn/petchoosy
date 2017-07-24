894f92d35b5b47d882789970d48a9cf5c710b4ce
Ashtn <ashtonehorton@gmail.com>
2017-07-24 14:25:10 -0700

    congif for hosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require('path'); // utilities for working with file directories
 var HtmlWebpackPlugin = require('html-webpack-plugin'); // aligns with dev dependency inside package.json // creates and index.html file and puts it inside the dist file, and includes the script in the index_bundle.js file.
+var webpack  = require('webpack');
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -23,3 +24,14 @@ module.exports = {
     })
   ]
 };
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+module.exports = config;
