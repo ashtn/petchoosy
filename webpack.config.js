4e0a2cfc48a9237b52f8ae892a9129ec39275d1d
Ashtn <ashtonehorton@gmail.com>
2017-07-25 06:14:58 -0700

    added loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ var config = {
   module: {
     rules: [
       {test: /\.(js)$/, use: 'babel-loader'},
-      {test: /\.css$/, use: ['style-loader', 'css-loader']}
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.(png|jpg)$/, use: 'file-loader?name=images/[name].[ext]' },
     ]
   },
   devServer: {
